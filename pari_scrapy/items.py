a65eb6bfa023971db22e2e92417e93f3f320f66b
Suganthi T <suganthikrishnavathi@gmail.com>
2013-07-05 15:49:50 +0530

    Converting datetime format after crawling

--- pari_scrapy/items.py
+++ pari_scrapy/items.py
@@ -1,5 +1,7 @@
 from scrapy.item import Item, Field
 from scrapy.contrib.loader.processor import MapCompose, Join, TakeFirst
+from datetime import datetime
+from django.utils import timezone
 
 
 def remove_empty(str):
@@ -9,12 +11,18 @@ def remove_empty(str):
 def strip(str):
     return str.strip().strip('- ')
 
+def change_date_format(str):
+    if str.isdigit():
+        date = datetime.strptime(str, '%Y%m%d').replace(tzinfo=timezone.utc)
+    else: 
+        date = datetime.strptime(str, '%B %d, %Y').replace(tzinfo=timezone.utc)
+    return unicode(date)
 
 class Article(Item):
     title = Field(input_processor=MapCompose(strip))
     link = Field()
     content = Field(input_processor=MapCompose(strip), output_processor=Join())
-    date = Field(input_processor=MapCompose(remove_empty), output_processor=Join())
-    location = Field(output_processor=TakeFirst())
+    date = Field(input_processor=MapCompose(remove_empty, change_date_format), output_processor=Join())
+    location = Field(input_processor=MapCompose(remove_empty), output_processor=TakeFirst())
     author = Field()
     keywords = Field()
